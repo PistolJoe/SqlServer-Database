509e212393f68c09d6b3a993666eaa2a5ef02675
Anand Joseph <ajoseph@provehonetworks.com.au>
2021-09-26 22:39:35 +1000

    Update with YearBuild and ModelName

--- SqlServerDatabase/Product.sql
+++ SqlServerDatabase/Product.sql
@@ -3,5 +3,7 @@
 	[Id] INT NOT NULL PRIMARY KEY, 
     [Name] VARCHAR(50) NULL, 
     [Cost] INT NULL, 
-    [Color] NVARCHAR(50) NULL
+    [Color] NVARCHAR(50) NULL, 
+    [YearBuild] NCHAR(10) NULL, 
+    [ModelName] NCHAR(10) NULL
 )
